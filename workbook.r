f77899a524aed8d9984e9edead5256cac9945d15
Randy Armknecht <rarmknecht@gmail.com>
2015-03-03 22:23:42 -0600

    Completed Three Swirl Sessions
    Initial Commit after 3 swirl sessions

---
+++ workbook.r
@@ -0,0 +1,12 @@
+# Randy Armknecht
+# 
+# Coursera - Getting and Cleaning Data
+#
+# Workbook
+setwd("C:/Users/ranarm01/Documents/Github/Coursera-DataScience-3")
+
+# SWIRL
+install.packages("swirl")
+library(swirl)
+install_from_swirl("Getting and Cleaning Data")
+swirl()
