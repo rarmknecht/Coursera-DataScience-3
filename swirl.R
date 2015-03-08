8e152db42cd77d3cbd7fa0f259b97ef797bf4587
Randy Armknecht <rarmknecht@gmail.com>
2015-03-07 22:35:46 -0600

    Separated swirl from workbook

--- swirl.R
+++ swirl.R
@@ -5,4 +5,8 @@
 # Workbook
 setwd("C:/Users/ranarm01/Documents/Github/Coursera-DataScience-3")
 
-
+# SWIRL
+install.packages("swirl")
+library(swirl)
+install_from_swirl("Getting and Cleaning Data")
+swirl()
