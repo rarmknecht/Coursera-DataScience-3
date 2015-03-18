5eed92ef2cd4a7dc4e2eb39ccb64ed55f8f27d62
Randy Armknecht <rarmknecht@gmail.com>
2015-03-17 21:40:48 -0500

    Workbook update

--- workbook.r
+++ workbook.r
@@ -3,5 +3,9 @@
 # Coursera - Getting and Cleaning Data
 #
 # Workbook
+# Windows
 setwd("C:/Users/ranarm01/Documents/Github/Coursera-DataScience-3")
+# Linux
+#setwd("/mnt/truecrypt11/coding/Coursera-DataScience-3")
+
 
